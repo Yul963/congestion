5f194aaae8cb49e17846a9efa483d1809bf1ffa4
yul <dbfrb9786@gmail.com>
2023-04-30 01:32:36 +0900

    function added

--- include/congestion_cal.hpp
+++ include/congestion_cal.hpp
@@ -7,7 +7,13 @@
 #include <condition_variable>
 
 cv::Mat getImage(std::string& img_path);
+
 torch::Tensor post_process(cv::Mat image);
+
 torch::jit::script::Module load_model(std::string& model_path);
+
 cv::Mat tensor_to_image(torch::Tensor tensor);
+
+float get_congestion(std::vector<cv::Mat> base_images, std::vector<cv::Mat> target_image);
+
 void process_image(std::queue<cv::Mat>& q, std::mutex& mtx, std::condition_variable& conv, std::string& model_path);
--- include/vid_processing.hpp
+++ include/vid_processing.hpp
@@ -5,4 +5,12 @@
 #include <mutex>
 #include <condition_variable>
 
-void process_video(std::queue<cv::Mat>& q, std::mutex& mtx, std::condition_variable& conv, std::string& url);
+struct cctv{
+    std::string url;
+    std::string name;
+    cv::Mat base_image;
+};
+
+void change_sec(int s);
+
+void process_video(std::queue<cv::Mat>& q, std::mutex& mtx, std::condition_variable& conv, struct cctv& info);
