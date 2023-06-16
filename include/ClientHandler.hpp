297433b20601f1d5372bfe20217e1920dd8ae5ab
morewisdom <57562540+DahyeLee2232@users.noreply.github.com>
2023-06-16 13:38:16 +0900

    add timestamp

--- include/ClientHandler.hpp
+++ include/ClientHandler.hpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <ctime>
 #include <boost/json.hpp>
+
+#define TIME_MAXSIZE = 80;
 
 using namespace std;
 
 // 혼잡도 정보를 처리하기 위한 사용자 정의 클래스
 struct CongestionPair{
-    string facility_name;
-    string name_tag;
-    double cngst_value;
+    string fname;
+    string nametag;
+    double value;
+    string timestamp;
 
     CongestionPair(){
-        facility_name = "";
-        name_tag = "";
-        cngst_value = 0.;
+        fname = "";
+        nametag = "";
+        value = 0.0;
     }
 
     CongestionPair(string facility_name, string name_tag, double cngst_value){
-    this->facility_name = facility_name;
-    this->name_tag = name_tag;
-    this->cngst_value = cngst_value;
+    this->fname = facility_name;
+    this->nametag = name_tag;
+    this->value = cngst_value;
+    strftime(this->timestamp, TIME_MAXSIZE, "%Z-%m-%d %H:%M:%S", time(NULL));    //2023-06-16 13:26:01
     }
 };
 
